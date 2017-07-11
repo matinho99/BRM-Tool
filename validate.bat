7ff2511ca9691eba9d12ed005fed416cdeb9ed92
bosowiecki <b.osowiecki@gmail.com>
2017-07-11 13:55:24 +0200

    Repo init

---
+++ validate.bat
@@ -0,0 +1,14 @@
+@echo off
+set DATETMP=%DATE%
+rem Mon 08/18/2014
+set TIMETMP=%TIME%
+rem 10:36:23.65
+set TIMETMP=%TIMETMP::=%
+set TIMETMP=%TIMETMP: =0%
+set TIMETMP=%TIMETMP:~0,6%
+set DATETMP=%DATETMP:/=%
+set DATETMP=%DATETMP:-=%
+set DATETMP=%DATETMP: =_%
+set TIMESTAMP=%DATETMP%_%TIMETMP%
+@echo on
+ant validate -v > "logs/validate_%TIMESTAMP%.log" 2>&1
